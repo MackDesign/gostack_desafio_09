ea91d2317dc1230413f64724ed8e6f3c4f3e8768
Mackdesign <marcio@design2u.com.br>
2020-10-23 03:53:53 -0300

    Finalizando desafio 09

---
+++ src/shared/infra/typeorm/migrations/1603392838616-CreateCustomers.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+// para resolver uuid_generate_v4() erro da migration  rodar CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+export default class CreateCustomers1603392838616
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'customers',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('customers');
+  }
+}
